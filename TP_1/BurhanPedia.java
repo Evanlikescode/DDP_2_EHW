b2bc8134bedd4ac619b1f62026cf78e93b8becb0
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-02-06 10:50:47 +0700

    first commit for TP_1

---
+++ TP_1/BurhanPedia.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class TemplateTP1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=============================================================");
+        System.out.println("\n  ____             _                 _____         _ _       \r\n" +
+                " |  _ \\           | |               |  __ \\       | (_)      \r\n" +
+                " | |_) |_   _ _ __| |__   __ _ _ __ | |__) |__  __| |_  __ _ \r\n" +
+                " |  _ <| | | | '__| '_ \\ / _` | '_ \\|  ___/ _ \\/ _` | |/ _` |\r\n" +
+                " | |_) | |_| | |  | | | | (_| | | | | |  |  __/ (_| | | (_| |\r\n" +
+                " |____/ \\__,_|_|  |_| |_|\\__,_|_| |_|_|   \\___|\\__,_|_|\\__,_|\r\n" +
+                "                                                             \r\n" +
+                "                                                             ");
+        System.out.println("=============================================================");
+        System.out.println("============== Selamat datang di Burhanpedia! ===============");
+        System.out.println("=============================================================");
+    }
+}
